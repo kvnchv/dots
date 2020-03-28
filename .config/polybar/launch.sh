b606f7f3dd84e216b751e90239c7f38fd924c7bd
Kevin Chu <youkevinchu@gmail.com>
2020-03-28 00:15:15 -0400

    initial add

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Script to launch polybar bars
+
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Load wal colors
+(cat ~/.cache/wal/sequences &)
+
+polybar --reload bar-laptop &
+
+# Choose the primary bar based on the type of computer
+#case $(cat /sys/class/dmi/id/chassis_type) in
+#  9|10|19)
+#    PRIMARY_BAR=bar-laptop
+#    ;;
+#  *)
+#    PRIMARY_BAR=bar-primary
+#    ;;
+#esac
+
+# Launch polybar on every monitor
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
+#  echo $m
+#  if [ ! -z "$(polybar --list-monitors | awk -v m="$m" '$0 ~ m && /primary/')" ]; then
+#    MONITOR=$m polybar --reload $PRIMARY_BAR &
+#    echo primary
+#  else
+#    MONITOR=$m polybar --reload bar-secondary &
+#    echo secondary
+#  fi
+#done
+
+#echo "Bars launched..."
